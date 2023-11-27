3ed2810d6f9a4e8536de695c37ac2d661d0823cf
srak <spencer.rak@gmail.com>
2023-11-27 18:07:12 -0500

    Move ./Main to chronos/Main

--- src/chronos/Main.java
+++ src/chronos/Main.java
@@ -1,28 +1,55 @@
+package chronos;
+
 import chronos.db.*;
 import chronos.exceptions.*;
 import chronos.models.*;
 
+import net.sourceforge.argparse4j.*;
+import net.sourceforge.argparse4j.inf.*;
+
+import java.util.Scanner;
+
+
 public class Main {
 
-    showHelp() {
+    void showHelp() {
     }
 
-    tabComplete() {
+    void tabComplete() {
     }
 
-    clearScreen() {
+    void clearScreen() {
     }
 
     public static void main(String[] args) {
+
+        ArgumentParser ap = ArgumentParsers.newFor("chronos").build();
+        ap.addArgument("--cli");
+        System.out.println("What");
+        try {
+            System.out.println(ap.parseArgs(args));
+        } catch(ArgumentParserException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        // end parsing
+
         ConnectionManager cm = new ConnectionManager();
-        cm.connect();
-        cm.setupTables();
+        try {
+            cm.connect();
+            cm.setupTables();
+        } catch (ChronosDatabaseException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        Scanner stdin = new Scanner(System.in);
+        String buf;
 
         while(true) {
             System.out.print("chronos> ");
-
+            buf = stdin.nextLine();
+            System.out.print("You entered "+buf);
         }
-
-        System.out.println("Welcome!");
     }
 }
