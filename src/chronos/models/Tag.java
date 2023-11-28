15c86bf79cee0bc8e3fb0cbb2abbeea16703a026
srak <spencer.rak@gmail.com>
2023-11-28 07:46:46 -0500

    Builder pattern

--- src/chronos/models/Tag.java
+++ src/chronos/models/Tag.java
@@ -1,9 +1,56 @@
 package chronos.models;
 
-public class Tag {
+import java.util.ArrayList;
+
+import chronos.models.Serialize;
+import chronos.models.Task;
+
+
+public class Tag implements Serialize {
     private int id;
     private String name;
+    private ArrayList<Task> tasks;
 
     public Tag() {
+    }
+
+    public static class Builder() {
+        private int id;
+        private String name;
+        private ArrayList<Task> tags;
+
+        public Builder id(int id) { this.id = id; return this; }
+        public Builder name(String name) { this.name = name; return this; }
+        public Builder tags(ArrayList<Task> tasks) {
+            this.tasks = tasks; return this;
+        }
+
+        public Tag build() {
+            return new Tag(this);
+        }
+    }
+
+    public Tag(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.tasks = builder.tasks;
+    }
+
+    @Override
+    public Tag fromColumns(HashMap<String, Object> args) {
+        return Tag.Builder()
+                .id(args.get("id"))
+                .name(args.get("name"))
+                .tasks(args.get("tasks"))
+                .build();
+    }
+
+    @Override
+    public HashMap<String, Object> toColumns() {
+        HashMap<String, Object> ret = new HashMap<String, Object>();
+        ret.put("id", this.id);
+        ret.put("name", this.name);
+        ret.put("tasks", this.tasks);
+        return ret;
     }
 }
--- src/chronos/models/Task.java
+++ src/chronos/models/Task.java
@@ -1,16 +1,110 @@
 package chronos.models;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 import chronos.models.Tag;
+import chronos.models.Serialize;
 
 
-public class Task {
+public class Task implements Serialize {
     private int id;
     private String name;
     private String text;
+    private boolean in_progress;
+    private LocalDateTime time_created;
+    private LocalDateTime time_stopped;
+    private LocalDateTime time_started;
+    private LocalDateTime time_worked;
+    private LocalDateTime due_by;
     private ArrayList<Tag> tags;
 
     public Task() {
+    }
+
+    public static class Builder() {
+        private int id;
+        private String name;
+        private String text;
+        private boolean in_progress = False;
+        private LocalDateTime time_created;
+        private LocalDateTime time_stopped;
+        private LocalDateTime time_started;
+        private LocalDateTime time_worked;
+        private LocalDateTime due_by;
+        private ArrayList<Tag> tags;
+
+        public Builder id(int id) { this.id = id; return this; }
+        public Builder name(String name) { this.name = name; return this; }
+        public Builder text(String text) { this.text = text; return this; }
+        public Builder in_progress(boolean in_progress) {
+            this.in_progress = in_progress; return this;
+        }
+        public Builder time_created(LocalDateTime time_created) {
+            this.time_created = time_created; return this;
+        }
+        public Builder time_stopped(LocalDateTime time_stopped) {
+            this.time_stopped = time_stopped; return this;
+        }
+        public Builder time_started(LocalDateTime time_started) {
+            this.time_started = time_started; return this;
+        }
+        public Builder time_worked(LocalDateTime time_worked) {
+            this.time_worked = time_worked; return this;
+        }
+        public Builder due_by(LocalDateTime due_by) {
+            this.due_by = due_by; return this;
+        }
+        public Builder tags(ArrayList<Tag> tags) {
+            this.tags = tags; return this;
+        }
+
+        public Task build() {
+            return new Task(this);
+        }
+    }
+
+    public Task(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.text = builder.text;
+        this.in_progress = builder.in_progress;
+        this.time_created = builder.time_created;
+        this.time_stopped = builder.time_stopped;
+        this.time_started = builder.time_started;
+        this.time_worked = builder.time_worked;
+        this.due_by = builder.due_by;
+        this.tags = builder.tags;
+    }
+
+    @Override
+    public Task fromColumns(HashMap<String, Object> args) {
+        return Task.Builder()
+                .id(args.get("id"))
+                .name(args.get("name"))
+                .text(args.get("text"))
+                .in_progress(args.get("in_progress"))
+                .time_created(args.get("time_created"))
+                .time_stopped(args.get("time_stopped"))
+                .time_started(args.get("time_started"))
+                .time_worked(args.get("time_worked"))
+                .due_by(args.get("due_by"))
+                .tags(args.get("tags"))
+                .build();
+    }
+
+    @Override
+    public HashMap<String, Object> toColumns() {
+        HashMap<String, Object> ret = new HashMap<String, Object>();
+        ret.put("id", this.id);
+        ret.put("name", this.name);
+        ret.put("text", this.text);
+        ret.put("in_progress", this.in_progress);
+        ret.put("time_created", this.time_created);
+        ret.put("time_stopped", this.time_stopped);
+        ret.put("time_started", this.time_started);
+        ret.put("time_worked", this.time_worked);
+        ret.put("due_by", this.due_by);
+        return ret;
     }
 }
