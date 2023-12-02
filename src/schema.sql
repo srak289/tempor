ce92bb76f7b28f463c65e2bbdc73cc1d4bd7d8a9
srak <spencer.rak@gmail.com>
2023-12-01 23:22:42 -0500

    Snapshot

--- src/schema.sql
+++ src/schema.sql
@@ -6,12 +6,13 @@ CREATE TABLE IF NOT EXISTS task (
     -- Effectively "in_progress" must be either NULL or True
     in_progress     BOOLEAN
         DEFAULT NULL UNIQUE CHECK (in_progress),
-    time_created    TIMESTAMP
-        NOT NULL,
+    time_created    TIMESTAMP,
     time_started    TIMESTAMP,
-    time_worked     TIMESTAMP,
+    time_worked     INTEGER
+        DEFAULT 0 NOT NULL,
     time_stopped    TIMESTAMP,
-    due_by          TIMESTAMP
+    due_by          TIMESTAMP,
+    allowed_time    INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS tag (
@@ -33,46 +34,61 @@ CREATE TABLE IF NOT EXISTS task_tags (
         ON DELETE CASCADE
 );
 
---CREATE TRIGGER IF NOT EXISTS trg_task_set_time_created
---    AFTER INSERT ON task
---    BEGIN
---        UPDATE task
---        SET time_created = CURRENT_TIMESTAMP
---        WHERE id = new.id;
---    END
---;
+CREATE TRIGGER IF NOT EXISTS trg_task_set_time_created
+    AFTER INSERT ON task
+    BEGIN
+        UPDATE task
+        SET time_created = CURRENT_TIMESTAMP
+        WHERE id = new.id;
+    END
+;
 
---CREATE TRIGGER IF NOT EXISTS trg_task_calculate_time_worked
---    AFTER UPDATE OF in_progress ON task
---    BEGIN
---        -- test this trigger
---        CASE WHEN (
---            old.in_progress = True
---            AND new.in_progress = NULL
---        ) THEN
---            UPDATE task
---            --TODO
---            SET time_stopped = CURRENT_TIMESTAMP,
---            time_worked = time_started - time_stopped
---            WHERE id = old.id;
---        ELSE
---            -- The only other state is to set from NULL to True
---            UPDATE task
---            SET time_started = CURRENT_TIMESTAMP
---            WHERE id = old.id;
---        END
---    END
---;
+-- TODO
+-- triggers for time stopped?
+
+CREATE TRIGGER IF NOT EXISTS trg_task_auto_stop_task
+    BEFORE UPDATE OF in_progress ON task
+    WHEN new.in_progress IS NULL
+    BEGIN
+        UPDATE task
+        SET
+            time_stopped = CURRENT_TIMESTAMP
+        WHERE id = old.id;
+    END
+;
+
+CREATE TRIGGER IF NOT EXISTS trg_task_calculate_time_worked
+    AFTER UPDATE OF in_progress ON task
+    WHEN new.in_progress IS NULL
+    BEGIN
+        UPDATE task
+        SET
+            time_worked = (old.time_worked + (old.time_started - old.time_stopped))
+        WHERE id = old.id;
+    END
+;
+
+CREATE TRIGGER IF NOT EXISTS trg_task_set_time_for_start
+    AFTER UPDATE OF in_progress ON task
+    WHEN new.in_progress
+    BEGIN
+        UPDATE task
+        SET
+            time_started = CURRENT_TIMESTAMP,
+            time_stopped = NULL
+        WHERE id = old.id;
+    END
+;
 
 CREATE VIEW IF NOT EXISTS task_join_tags AS
     SELECT ta.name AS task_name, GROUP_CONCAT(tg.name) AS task_tags
         FROM task AS ta
-        JOIN task_tags ON task_id=ta.id
-        JOIN tag AS tg ON tag_id=tg.id
+        JOIN task_tags ON task_id = ta.id
+        JOIN tag AS tg ON tag_id = tg.id
         GROUP BY ta.name
 ;
 
---CREATE VIEW IF NOT EXISTS time_spent AS
+--CREATE VIEW IF NOT EXISTS time_per_tag AS
 --    -- TODO
 --    SELECT * from task
 --;
--- src/test.sql
+++ src/test.sql
@@ -1,13 +1,13 @@
 BEGIN;
 
-INSERT INTO task (id, name, in_progress, time_created) VALUES
-    (0, 'Take out trash', True, CURRENT_TIMESTAMP),
-    (1, 'Do homework', NULL, CURRENT_TIMESTAMP),
-    (2, 'Sleep', NULL, CURRENT_TIMESTAMP),
-    (4, 'Eat breakfast', NULL, CURRENT_TIMESTAMP),
-    (5, 'Video games', NULL, CURRENT_TIMESTAMP),
-    (6, 'Make coffee', NULL, CURRENT_TIMESTAMP),
-    (3, 'Eat dinner', NULL, CURRENT_TIMESTAMP);
+INSERT INTO task (id, name) VALUES
+    (0, 'Take out trash'),
+    (1, 'Do homework'),
+    (2, 'Sleep'),
+    (4, 'Eat breakfast'),
+    (5, 'Video games'),
+    (6, 'Make coffee'),
+    (3, 'Eat dinner');
 
 INSERT INTO tag (id, name) VALUES
     (0, 'High Importance'),
@@ -26,5 +26,7 @@ INSERT INTO task_tags (task_id, tag_id) VALUES
     (4, 2),
     (5, 0),
     (3, 1);
+
+UPDATE task SET in_progress = True WHERE id = 5;
 
 COMMIT;
